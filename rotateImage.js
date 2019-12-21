67a8dbe3b6d3f51e5551ee1e7fd061da52259213
Brady <brady.y.liu@gmail.com>
2019-12-20 23:02:30 -0800

    complete rotateImage

--- rotateImage.js
+++ rotateImage.js
@@ -1,49 +1,41 @@
 var rotate = function (matrix) {
-  let traversed = {};
-  let holder;
-  const goToNext = (x, y, nextValue, cycle = 0) => {
-    if(traversed[`${x} + ${y}`] !== undefined) return;
-    if(cycle === 0) {
-      traversed[`${matrix.length - 1 - x} + ${y}`] = 0;
-      holder = matrix[y][matrix.length - 1 - x];
-      matrix[y][matrix.length - 1 - x] = nextValue;
-      goToNext(x, y, holder, 1);
-    } else if (cycle === 1) {
-      traversed[`${matrix.length - 1 - x} + ${matrix.length - 1 - y}`] = 0;
-      holder = matrix[matrix.length - 1 - y][matrix.length - 1 - x];
-      console.log(holder);
-      matrix[matrix.length - 1 - y][matrix.length - 1 - x] = nextValue;
-      goToNext(x, y, holder, 2);
-    } else if (cycle === 2) {
-      traversed[`${x} + ${matrix.length - 1 - y}`] = 0;
-      holder = matrix[matrix.length - 1 - y][x];
-      matrix[matrix.length - 1 - y][x] = nextValue;
-      goToNext(x, y, holder, 3);
-    } else if (cycle === 3) {
-      traversed[`${x} + ${y}`] = 0;
-      matrix[y][x] = nextValue;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i; j < matrix[0].length; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
     }
   }
-  for(let i = 0; i < matrix.length; i++) {
-    for(let k = 0; k < matrix.length; k++) {
-      goToNext(i, k, matrix[i][k], i);
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length / 2; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[i][matrix[0].length - j - 1];
+      matrix[i][matrix[0].length - j - 1] = temp;
     }
   }
-  for(let i = 0; i < matrix.length; i++) {
-    console.log(matrix[i]);
-  }
-  return
+  return matrix;
 };
 
+const test0 = [
+  [0, 1],
+  [2, 3]
+]
 const test1 = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
 ];
 const test2 = [
-  [ 5, 1, 9,11],
-  [ 2, 4, 8,10],
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
   [13, 3, 6, 7],
-  [15,14,12,16]
+  [15, 14, 12, 16]
 ];
-console.log(rotate(test1));
+const test3 = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15,],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25]
+]
+console.log(rotate(test3));
