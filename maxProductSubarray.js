be34f97ec2630e74bf268e7fba1a34cabcaf571c
Brady <brady.y.liu@gmail.com>
2019-12-21 18:24:03 -0800

    o(n) solution

--- maxProductSubarray.js
+++ maxProductSubarray.js
@@ -1,11 +1,33 @@
 var maximumProduct = function(nums) {
-  let s = nums.sort((a,b) => a - b), product;
-  if(s[0] * s[1] > s[s.length - 3] * s[s.length - 2]) {
-    product = s[0] * s[1] * s[s.length - 1]
-  } else {
-    product = s[s.length - 3] * s[s.length - 2] * s[s.length - 1]
+  let a = b = c = -Infinity;
+  let x = y = +Infinity;
+
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] > a) {
+      c = b;
+      b = a;
+      a = nums[i];
+    } else if (nums[i] > b) {
+      c = b;
+      b = nums[i];
+    } else if (nums[i] > c) {
+      c = nums[i]
+    }
+    if(nums[i] < x) {
+      y = x;
+      x = nums[i]
+    } else if (nums[i] < y) {
+      y = nums[i];
+    }
   }
-  return product;
+  return Math.max(a*b*c, a*x*y);
+  // let s = nums.sort((a,b) => a - b), product;
+  // if(s[0] * s[1] > s[s.length - 3] * s[s.length - 2]) {
+  //   product = s[0] * s[1] * s[s.length - 1]
+  // } else {
+  //   product = s[s.length - 3] * s[s.length - 2] * s[s.length - 1]
+  // }
+  // return product;
 };
 
 const test1 = [1,2,3]; // 6
