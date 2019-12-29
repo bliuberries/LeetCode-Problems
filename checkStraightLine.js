2a1998d8f9decac10559205baecc8cd99c458d95
Brady <brady.y.liu@gmail.com>
2019-12-28 23:08:57 -0800

    Complete checkStraightLine

--- checkStraightLine.js
+++ checkStraightLine.js
@@ -3,10 +3,11 @@
  * @return {boolean}
  */
 var checkStraightLine = function (coords) {
-  for (let i = 1; i < coords.length; i++) {
-    let difx = coords[i][0] - coords[i - 1][0];
-    let dify = coords[i][1] - coords[i - 1][1];
-    if (difx !== dify) return false; // diag lines
+  if (coords.length < 2) return true;
+  let slope = (coords[1][1] - coords[0][1]) / (coords[1][0] - coords[0][0]);
+  for (let i = 2; i < coords.length; i++) {
+    let s = (coords[i][1] - coords[0][1]) / (coords[i][0] - coords[0][0])
+    if (s !== slope) return false;
   }
   return true;
 };
@@ -14,6 +15,6 @@ var checkStraightLine = function (coords) {
 let test1 = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]; //true
 let test2 = [[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]; //false
 let test3 = [[-3, -2], [-1, -2], [2, -2], [-2, -2], [0, -2]];
-let test4 = [[0,1],[1,3],[-4,-7],[5,11]];
+let test4 = [[0, 1], [1, 3], [-4, -7], [5, 11]];
 console.log(checkStraightLine(test3));
 // console.log(checkStraightLine(test2));
